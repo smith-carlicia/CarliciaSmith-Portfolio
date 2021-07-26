e288e914d0f77470f12ac3099337e6d62339085d
Carlicia Smith <smith.carlicia@gmail.com>
2021-07-26 09:38:39 -0400

    removed name brand in navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,28 +1,72 @@
-import React from 'react';
-import { Toolbar } from '@material-ui/core';
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+ // import { Toolbar } from '@material-ui/core';
+import { Link, HashRouter, Route } from "react-router-dom";
+// import { Link } from 'react-scroll';
+import Home from '../../pages/Home/Home';
+import About from '../../pages/About/About';
+import Portfolio from '../../pages/Portfolio/portfolio';
+import Contact from '../../pages/Contact/contact';
 import './Nav.css';
+  
+class Navigation extends Component{
 
-const Navigation=()=>{
-    
-    // const location = useLocation();
+    render() {
+         // const location = useLocation();
 
     return(
-            <Toolbar direction="row" className="navi">
-                <h3 className="carly">Carlicia</h3>
-                <Link className="link" to="/">
-                    <h5>Home</h5>
-                </Link>
-                <Link className="link" to="About">
-                    <h5>About</h5>
-                </Link>
-                <Link className="link" to="Portfolio">
-                    <h5>Portfolio</h5>
-                </Link>
-                <Link className="link" to="Contact">
-                    <h5>Contact</h5>
-                </Link>
-            </Toolbar> 
+        <HashRouter>
+           <>
+                 <Navbar 
+            className="navbar" 
+            // bg="light" 
+            // variant="light"
+            >
+                <Nav 
+                    className="mr-auto">
+                        <Link 
+                            className="link" 
+                            to="/" >
+                                <h6>
+                                    Home
+                                </h6>
+                        </Link>
+                        <Link 
+                            className="link" 
+                            to="About">
+                                <h6>
+                                    About
+                                </h6>
+                        </Link>
+                        <Link 
+                            className="link" 
+                            to="Portfolio">
+                                <h6>
+                                    Portfolio
+                                </h6>
+                        </Link>
+                        <Link
+                            className="link" 
+                            to="Contact">
+                                <h6>
+                                    Contact
+                                </h6>
+                        </Link>
+                </Nav>
+        </Navbar>
+           </>
+           <>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+           </>
+    </HashRouter>          
     )
+    }
+   
+    
+   
 }
 export default Navigation;

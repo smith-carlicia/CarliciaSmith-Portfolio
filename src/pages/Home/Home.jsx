6303432f38a18e8dd76b0b68a3b3730be683a89d
Carlicia Smith <smith.carlicia@gmail.com>
2021-07-26 09:47:43 -0400

    changed body color to white

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,55 +1,105 @@
-import React from 'react'
-import { Grid, Button } from '@material-ui/core';
+import React, { Component } from "react";
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import graph from '../../utils/Img/port-logo.jpeg'
+// import { Grid, Button } from '@material-ui/core';
 import './Home.css';
 
-function Home() {
-    return(
-        <Grid>
-        <Grid container>
-            <Grid xs={1}></Grid>
-                <Grid item direction="row" xs={10}>
-                    <h1 className="header">C a r l i c i a   S m i t h</h1>
-                    <h3 className="roles">Software Engineer | Full Stack Web Developer</h3>
-                </Grid>
-            <Grid xs={1}></Grid>
-        <Grid container>
-            <Grid xs={8}></Grid>
-            <Grid xs={4}>
-              <Button 
-                        className="resume" 
-                        variant="contained" 
-                        color="secondary"
-                        justify="center"
-                        alignItems="center"
-                        size="large"
-                        href="https://drive.google.com/file/d/1_qATeK_qcJb_ZE6JdkaH0vzPz-RqnLzc/view?usp=sharing" 
-                        target="_blank">
-                        Resume
-                    </Button>
-            </Grid>
-        </Grid>
-        </Grid>
-        </Grid>
-    //    <div>
-    //        <Row>
-    //        {/* <header> */}
-    //            <Col sm="7">
-    //            <h1>Carlicia Smith</h1>
-    //            </Col>
-    //            <Col sm="5">
-    //            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHlSIGXxvUVSg/profile-displayphoto-shrink_200_200/0/1579299268559?e=1613606400&v=beta&t=I6v52097UlqmkskL2M4jEXnyRV84TsbKgI3Kd3iFkXw" 
-    //             class="rounded-circle"
-    //             alt="carly-pro"
-    //             width="250"
-    //             height="236"
-    //            />
-               
-    //        </Col>
-    //        {/* <Col sm="1"></Col> */}
-    //        {/* </header> */}
-    //        </Row>
-    //    </div>
+class Home extends Component {
+    render() {
+        return(
+        <>
+        <Container
+            className="home" 
+            fluid>
+                <Row className="justify-content-md-center">
+                    {/* <Col xs md lg={2} /> */}
+                     {/* <Col xs md lg={6} className="photo />  */}
+
+                    <Col xs md lg={12}>
+                       <h3
+                            className="carlicia">
+                                C  a  r  l  i  c  i  a   S  m  i  t  h
+                        </h3>
+                           <p 
+                            className="seRoles">
+                                Software Engineer | Full Stack Web Developer
+                            </p>
+                        <Button 
+                            className="resume" 
+                            variant="outline-dark"
+                            justify="center"
+                            alignItems="center"
+                            size="large"
+                            href="https://drive.google.com/file/d/1_qATeK_qcJb_ZE6JdkaH0vzPz-RqnLzc/view?usp=sharing" 
+                            target="_blank">
+                                Resume
+                        </Button>
+                    </Col>
+                    {/* <Col xs md lg={2} /> */}
+
+                        
+                    {/* <Col lg={4}>
+                        <Button 
+                            className="resume" 
+                            variant="outline-dark"
+                            justify="center"
+                            alignItems="center"
+                            size="large"
+                            href="https://drive.google.com/file/d/1_qATeK_qcJb_ZE6JdkaH0vzPz-RqnLzc/view?usp=sharing" 
+                            target="_blank">
+                                Resume
+                        </Button>
+                    </Col> */}
+                </Row>
+
+
+                {/* <Row>
+                    <Col lg={8}></Col>
+                    <Col lg={4}>
+                        <Button 
+                            className="resume" 
+                            variant="outline-dark"
+                            justify="center"
+                            alignItems="center"
+                            size="large"
+                            href="https://drive.google.com/file/d/1_qATeK_qcJb_ZE6JdkaH0vzPz-RqnLzc/view?usp=sharing" 
+                            target="_blank">
+                                Resume
+                        </Button>
+                    </Col>
+                // </Row> */}
+        </Container>
+        </>
+        // <Grid id="/">
+        // <Grid container>
+        //     <Grid xs={1}></Grid>
+        //         <Grid item direction="row" xs={10}>
+        //             <h1 className="header">C a r l i c i a   S m i t h</h1>
+        //             <h3 className="roles">Software Engineer | Full Stack Web Developer</h3>
+        //         </Grid>
+        //     <Grid xs={1}></Grid>
+        // <Grid container>
+        //     <Grid xs={8}></Grid>
+        //     <Grid xs={4}>
+        //       <Button 
+        //                 className="resume" 
+        //                 variant="outline-info"
+        //                 justify="center"
+        //                 alignItems="center"
+        //                 size="large"
+        //                 href="https://drive.google.com/file/d/1_qATeK_qcJb_ZE6JdkaH0vzPz-RqnLzc/view?usp=sharing" 
+        //                 target="_blank">
+        //                 Resume
+        //             </Button>
+        //     </Grid>
+        // </Grid>
+        // </Grid>
+        // </Grid>
     )
+    }
 }
 
 export default Home;
